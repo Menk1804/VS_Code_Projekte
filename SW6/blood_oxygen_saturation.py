eb63880d25bbda77e293597c961624a0f3bee0ef
Menk von Weissenfluh <vonwemel@students.zhaw.ch>
2025-10-24 12:55:18 +0200

    Gelöscht

--- SW6/blood_oxygen_saturation.py
+++ SW6/blood_oxygen_saturation.py
@@ -1,17 +1,5 @@
 
 
-# import csv
-# import os
-
-# # Bestimme automatisch den Pfad zur CSV-Datei im gleichen Ordner wie das Skript
-# script_dir = os.path.dirname(__file__)
-# file_path = os.path.join(script_dir, 'saturation_per_day.csv')
-
-# # Öffne die CSV-Datei und lese sie Zeile für Zeile ein
-# with open(file_path, newline='', encoding='utf-8') as csv_datei:
-#     csv_reader = csv.reader(csv_datei)
-#     for zeile in csv_reader:
-#         print(zeile)
 
 import csv
 import os
