604a0683f231053a1aad970f8108fea3302bcc41
Menk von Weissenfluh <vonwemel@students.zhaw.ch>
2025-10-26 10:05:08 +0100

    übungen

---
+++ SW6/übungen_leistungsnachweis_2.py
@@ -0,0 +1,14 @@
+# #übung mit while schleiffe
+
+# zahl = 2
+
+# while zahl <=12:
+#     print(zahl + 2)
+
+# zahl = 1
+
+# while zahl <= 5:
+#     print("Aktuelle Zahl:", zahl)
+#     zahl = zahl + 1  # oder: zahl += 1
+
+print('hallo')
