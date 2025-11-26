0106b666c7e2d3c413dbd0ea02c806690be2b7de
Menk von Weissenfluh <vonwemel@students.zhaw.ch>
2025-11-26 10:02:33 +0100

    übung mit Schere stein Papier

--- Programmieren/SW8/schere-stein-papier.py
+++ Programmieren/SW8/schere-stein-papier.py
@@ -73,4 +73,85 @@ def anzeigen(wahl, invers=False):
 #==================================
 #=== Hier beginnt die Challenge ===
 #==================================
+# auswahl
+auswahl = ['SCHERE', 'STEIN', 'PAPIER']
+
+# start
+print('')
+print('''=================================''')
+print('''   SCHERE <-> STEIN <-> PAPIER   ''')
+print('''=================================''')
+print('')
+
+# frage nach der benötigten anzahl punkte für den sieg
+print('Auf wieviele Siegpunkte soll gespielt werden?')
+sieg_punkte = int(input('Bitte eine ganze Zahl eingeben (z.B. 5) : '))
+
+# initialisierung der punkte
+punkte_spielerin = 0
+punkte_computer  = 0
+
+# auswertung
+def auswertung(wahl_1, wahl_2):
+
+    # vergleich
+    if wahl_1==wahl_2:
+        print('==> Unentschieden')
+        return 0, 0
+    elif wahl_1==1 and wahl_2==3:
+        print('==> Du hast diese Runde gewonnen!')
+        return 1, 0
+    elif wahl_1==2 and wahl_2==1:
+        print('==> Du hast diese Runde gewonnen!')
+        return 1, 0
+    elif wahl_1==3 and wahl_2==2:
+        print('==> Du hast diese Runde gewonnen!')
+        return 1, 0
+    else:
+        print('==> Diese Runde geht an den Computer')
+        return 0, 1
+
+# spiel schleife
+while True:
+
+    # zug spieler:in
+    print('''Bitte wähle:
+  - Schere: 1
+  - Stein : 2
+  - Papier: 3''')
+    wahl_spielerin = int(input('Deine Wahl: '))
+    print(f'''Du hast {auswahl[wahl_spielerin-1]} gewählt:''')
+    anzeigen(wahl_spielerin)
+
+    # zug computer
+    wahl_computer = random.choice([1, 2, 3])
+    print(f'''Der Computer hat {auswahl[wahl_computer-1]} gewählt:''')
+    anzeigen(wahl_computer, True)
+
+    # runde auswerten
+    punkt_spielerin, punkt_computer = auswertung(wahl_spielerin, wahl_computer)
+    punkte_spielerin = punkte_spielerin + punkt_spielerin
+    punkte_computer = punkte_computer + punkt_computer
+
+    # aktuellen zwischenstand anzeigen
+    print(' -------------------------')
+    print('| Aktueller Zwischenstand |')
+    print(f'''|  Du: {punkte_spielerin}  |  Computer: {punkte_computer}  |''')
+    print(' -------------------------')
+    print('')
+
+    # spiel fertig?
+    if punkte_spielerin>=sieg_punkte:
+        print('================================')
+        print(' GRATULATION - Du hast gewonnen!')
+        print('================================')
+        print('')
+        break
+    if punkte_computer>=sieg_punkte:
+        print('=======================================')
+        print(' Ah, Mist! - Der Computer hat gewonnen!')
+        print('=======================================')
+        print('')
+        break
+
 
