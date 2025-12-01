ab6f5cce27d6b0aa0201440204814b40d6986e09
Menk von Weissenfluh <vonwemel@students.zhaw.ch>
2025-12-01 14:20:05 +0100

    übungen sind fertig

---
+++ Programmieren/SW09/übung_mit_random.py
@@ -0,0 +1,45 @@
+# Importiert das Modul random für die weiteren Übungen
+
+import random
+
+
+# # Zeigt die vollständige Dokumentation des Moduls.
+
+# help(random)
+
+## listet alle Attribute, Klassen und Funktione des Moduls.
+
+# dir(random)
+
+
+
+# # Gibt Informationen nur zu einer bestimmten Funktion
+
+# help(random.sample)
+
+
+# # Zeigt an wo sich das Modul random auf meiner Festblatte befindet.
+
+# print(random.__file__)
+
+
+# # Prüft ob random zu den built-in Modulen gehört?
+
+# import sys
+# print('random' in sys.builtin_module_names)
+
+
+# Testet ob es sich um Moudle oder Packete handelt.
+
+import xml
+xml.__file__
+
+import sys
+'xml' in sys.builtin_module_names
+
+import sys
+sys.__file__
+
+
+
+
