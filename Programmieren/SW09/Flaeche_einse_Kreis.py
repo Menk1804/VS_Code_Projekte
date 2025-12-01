045cd7d75bbc33a820ec9fad3e106f8b7fcbca79
Menk von Weissenfluh <vonwemel@students.zhaw.ch>
2025-12-01 13:36:42 +0100

    Vertige Aufgabe

--- Programmieren/SW09/Flaeche_einse_Kreis.py
+++ Programmieren/SW09/Flaeche_einse_Kreis.py
@@ -1,4 +1,23 @@
 # Importiert die Module für die Berechnungen
 import random as rnd
-from math import pi, squrt as sr
+from math import pi, sqrt as sr
+
+# Erzeugt einen Zufälligen Radius zwischen 1-10
+
+n = rnd.randint(1,10) #ganze Zahl zwischen 1 und 10
+radius = n ** 2  # Quadriert den Radius
+
+#Formel für die Kreisfläche
+circle_area = pi * radius ** 2
+
+#Seitenlänge des Dreicks berechnen
+side_length = sr(4 * circle_area / sr(3))
+
+# Höhe des Dreiecks berechnen
+hight = side_length * sr(3) / 2
+
+# Ausgabe der Ergebnisse im Terminal
+print("Zufälliger Radius des Kreises:", radius)
+print("Seitenlänge des gleichseitigen Dreiecks:", side_length)
+print("Höhe des gleichseitigen Dreiecks:", hight)
 
